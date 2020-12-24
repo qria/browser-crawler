31d2da0354e7ea97fb2af8cda16ba492a3e7fe9f
Qria <qria@qria.net>
2020-12-24 10:20:43 +0900

    Download crawlled data as excel

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,7 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+import ExcelJS from 'exceljs';
+
 
 const interestedTabs = [];
 const data = [];
@@ -25,12 +27,34 @@ chrome.runtime.onMessage.addListener(
     }
 );
 
+function downloadAsExcel(rows) {
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet('data');
+    worksheet.columns = [
+        {header: '제목', key: 'title'},
+        {header: '링크', key: 'link'},
+    ]
+    worksheet.addRows(rows);
+
+    workbook.xlsx.writeBuffer( {
+        base64: true
+    })
+    .then( function (xls64) {
+        const blob = new Blob([xls64], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
+        const url = URL.createObjectURL(blob);
+        chrome.downloads.download({url: url, filename: "data.xlsx"});
+    });
+}
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (interestedTabs.includes(tabId) && changeInfo.status == 'complete') {
         chrome.tabs.sendMessage(tabId, {crawl: true}, function(response) {
             // remove tab from interested tabs
             interestedTabs.splice(interestedTabs.indexOf(tabId), 1);
             data.push(response.data);
+
+            // Download data as file
+            downloadAsExcel(data[0].articles);
         });
     }
 });
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,13 +1,11 @@
 import "../css/bootstrap/bootstrap.css";
 import "../css/popup.css";
-import ExcelJS from 'exceljs';
 
 
 document.addEventListener("DOMContentLoaded", function() {
     let downloadButton = document.getElementById('downloadButton');
 
     downloadButton.onclick = function(element) {
-        // const workbook = new ExcelJS.Workbook();
         chrome.runtime.sendMessage({work: true})
     };
 });
